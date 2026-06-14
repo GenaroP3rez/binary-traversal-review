54950ead8a9ec0ec5d1ea492e26326630f7e0879
lilPunny <166659824+GenaroP3rez@users.noreply.github.com>
2026-06-13 19:07:35 -0700

    q7 and 8 will fix l8er

--- src/TraversalReview.java
+++ src/TraversalReview.java
@@ -265,7 +265,16 @@ public class TraversalReview {
      * @return whether every value is divisible by k
      */
     public static boolean hasNonDivisible(TreeNode node, int k) {
+        
+        if (node = null){
         return false;
+    }
+    if(node.data % k != 0){
+        return true;
+    }
+
+    return hasNonDivisible(node.left, k)
+            || hasNonDivisible(node.right, k)
     }
 
     /**
@@ -291,6 +300,15 @@ public class TraversalReview {
      * @return a string with all the values of the tree concatenated in-order
      */
     public static String concatenate(TreeNode node) {
-        return "";
+        //return "";
+        if (node == null){
+            return "";
+        }
+
+
+        return concatenate(node.left )
+        + node.data
+        + concatenate(node.right);
+        
     }
 }
